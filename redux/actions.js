0eaca501e9511ce4ac436d1a33c756b686ee0c4b
Koby Conrad <52094714+kobyconrad@users.noreply.github.com>
2020-03-02 16:25:08 -0800

    created actions

---
+++ redux/actions.js
@@ -0,0 +1,33 @@
+import { createStore } from "redux";
+import { ADD_TODO, REMOVE_TODO } from "../actionTypes";
+
+const ADD_TODO = "ADD_TODO";
+const TOGGLE_TODO = "TOGGLE_TODO";
+const SET_VISIBILITY_FILTER = "SET_VISIBILITY_FILTER";
+
+export const VisibilityFilters = {
+  SHOW_ALL: "SHOW_ALL",
+  SHOW_COMPLETED: "SHOW_COMPLETED",
+  SHOW_ACTIVE: "SHOW_ACTIVE"
+};
+
+export function addTodo(text) {
+  return {
+    type: ADD_TODO,
+    text
+  };
+}
+
+export function toggleTodo(index) {
+  return {
+    type: TOGGLE_TODO,
+    index
+  };
+}
+
+export function setVisibilityFilter(filter) {
+  return {
+    type: SET_VISIBILITY_FILTER,
+    filter
+  };
+}
